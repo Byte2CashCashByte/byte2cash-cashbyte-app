38e3f2b9e3ea8f6ad21d5e0d71dc8a1d19f7e512
Byte2CashCashByte <abdulwasiu9896@gmail.com>
2025-09-19 11:03:39 +0100

    Add signup page

---
+++ signup.js
@@ -0,0 +1,13 @@
+export default function Signup() {
+  return (
+    <div>
+      <h1>Signup</h1>
+      <form>
+        <input type="text" placeholder="Name" required />
+        <input type="email" placeholder="Email" required />
+        <input type="password" placeholder="Password" required />
+        <button type="submit">Signup</button>
+      </form>
+    </div>
+  );
+}
