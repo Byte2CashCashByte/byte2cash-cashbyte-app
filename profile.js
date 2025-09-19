b0e788066976daf04b2dde7d333bc7130434365e
Byte2CashCashByte <abdulwasiu9896@gmail.com>
2025-09-19 11:06:50 +0100

    Add user profile page

---
+++ profile.js
@@ -0,0 +1,10 @@
+export default function Profile() {
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <input type="text" placeholder="Display Name" />
+      <input type="file" />
+      <button>Update Profile</button>
+    </div>
+  );
+}
