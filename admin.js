f76da8d4adce970cb2db769f0d520b997e844850
Byte2CashCashByte <abdulwasiu9896@gmail.com>
2025-09-19 11:08:29 +0100

    Add admin dashboard page

---
+++ admin.js
@@ -0,0 +1,8 @@
+export default function Admin() {
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      <p>Here you can see transactions, commissions, and user list.</p>
+    </div>
+  );
+}
